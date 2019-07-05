3946570aa4f20b07111964396aa9b7817fd7bec5
vlatkotodorovski <vlatko_ku@hotmail.com>
2019-07-05 14:37:33 +0200

    delete commented code for alert component

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,21 +1,7 @@
 import React from 'react'
 
 export class Alert extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
 
-    //         products: [],
-    //         visable: false
-            
-
-    //     }
-    //     // this.HandleFieldsChange = this.HandleFieldsChange.bind(this);
-    //     // this._renderExpenses = this._renderExpenses.bind(this);
-    //     // this.onNavigateNewProduct = this.onNavigateNewProduct.bind(this);
-    //     // this.onNavigateNewCalc = this.onNavigateNewCalc.bind(this);
-    //     // this._NavigateEditProduct = this._NavigateEditProduct.bind(this);
-    // }
 
     render() {
         return (
@@ -26,8 +12,6 @@ export class Alert extends React.Component {
                         <p>You are about to delete this product. Are you sure you wish to continue?</p>
                     </div>
                     <div className="alert-btns">
-                        {/* <button className="cancel-btn">CANCEL</button> */}
-                        {/* <button className="del-btn">DELETE</button> */}
                         <button className="cancel-btn" onClick={this.props.visibleFalse}>CANCEL</button>
                         <button className="del-btn" onClick={this.props._deleteProduct}>DELETE</button>
                     </div>
